6a23898730dddebc46d747fb9e73d86023a42f7a
Gabriel Pinheiro <pmgabriel54@gmail.com>
2023-08-13 12:47:12 -0300

    Adicionando o restante das informações sobre a Costa Rica

--- producao-midiatica/index.js
+++ producao-midiatica/index.js
@@ -1,3 +1,11 @@
 //IDEIAS!!!
 
-//QUANDO CLICAR NO NOME DA BANDA OU CANTOR OU CANAL DE TELEVISÃO, APARECER UMA IMAGEM ILUSTRANDO AQUILO
+//TROCAR OS TEXTOS POR IMAGENS, E QUANDO CLICAR NELAS, FAZER OS TEXTOS APARECEREM.
+//USAR UMA FONTE "ESCRITA À MÃO" NAS INFORMAÇÕES SOBRE CANAIS, CANTORES E ETC.
+
+const imagens = document.querySelectorAll(".item-imagem");
+
+imagens.forEach((imagem) => imagem.addEventListener("click", () => 
+{
+    imagem.parentNode.querySelector(".item-texto").classList.toggle("hidden");
+}))
